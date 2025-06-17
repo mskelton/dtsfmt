a6c70fd39567d508ba2e2698ef66a833d71c916e
Marcel Hellwig <github@cookiesoft.de>
2025-06-17 12:24:44 +0200

    update dependencies
    Updated toml to 0.8 and tree-sitter to 0.25

--- src/parser.rs
+++ src/parser.rs
@@ -8,6 +8,6 @@ pub fn parse(source: String) -> Tree {
     let language = unsafe { tree_sitter_devicetree() };
     let mut parser = Parser::new();
 
-    parser.set_language(language).unwrap();
+    parser.set_language(&language).unwrap();
     parser.parse(source, None).unwrap()
 }
